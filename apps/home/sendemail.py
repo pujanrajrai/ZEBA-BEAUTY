42bd39edb3b1de7c3fb328acfa1d2489322dec2f
Pujan Raj Rai <70184988+pujanrajrai@users.noreply.github.com>
2024-09-24 01:23:36 +0545

    done

--- apps/home/sendemail.py
+++ apps/home/sendemail.py
@@ -5,6 +5,7 @@ from home.models import Email
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+import threading
 
 
 class SendEmail:
@@ -12,22 +13,37 @@ class SendEmail:
         self.host_email = EMAIL_HOST_USER
 
     def send_email_to_client(self, email, subject, body_var):
-        html_content = render_to_string('email.html', body_var)
-        text_content = strip_tags(html_content)
+        def send():
 
-        msg = EmailMultiAlternatives(
-            subject, text_content, EMAIL_HOST_USER, email)
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
+            html_content = render_to_string('email.html', body_var)
+            text_content = strip_tags(html_content)
+            msg = EmailMultiAlternatives(
+                subject, text_content, self.host_email, email)
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+        threading.Thread(target=send).start()
 
     def send_email_to_admin(self, subject, body_var):
-        html_content = render_to_string('admin_email.html', body_var)
-        text_content = strip_tags(html_content)
-        recipient_list = []  # Add your own email address here
-        other_email = Email.objects.all()
-        for email in other_email:
-            recipient_list.append(email.email)
-        msg = EmailMultiAlternatives(
-            subject, text_content, EMAIL_HOST_USER, recipient_list)
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
+        def send():
+            html_content = render_to_string('admin_email.html', body_var)
+            text_content = strip_tags(html_content)
+
+            # Create a flat list of email addresses
+            recipient_list = [
+                email.email for email in Email.objects.all() if email.email]
+
+            if recipient_list:
+                for recipient in recipient_list:
+                    try:
+                        msg = EmailMultiAlternatives(
+                            subject, text_content, self.host_email, [recipient])
+                        msg.attach_alternative(html_content, "text/html")
+                        msg.send()
+                        print(f"Email sent to {recipient}")
+                    except Exception as e:
+                        print(f"Failed to send email to {recipient}: {e}")
+            else:
+                print("No valid recipients found.")
+
+        threading.Thread(target=send).start()
